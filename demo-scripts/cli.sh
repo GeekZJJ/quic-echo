d2b0faa6a0f0c997aaf01ad8ecc2927a1a74745a
Daiki Ueno <ueno@gnu.org>
2021-08-30 19:36:37 +0200

    demo-scripts: Don't expand keylog path

--- demo-scripts/cli.sh
+++ demo-scripts/cli.sh
@@ -7,14 +7,18 @@ clear
 TYPE_SPEED=20
 
 export SSLKEYLOGFILE=$PWD/keylog.txt
-pei "export SSLKEYLOGFILE=$PWD/keylog.txt"
+pei 'export SSLKEYLOGFILE=$PWD/keylog.txt'
 
 export G_MESSAGES_DEBUG=echo
-pei "export G_MESSAGES_DEBUG=echo"
+pei 'export G_MESSAGES_DEBUG=echo'
 
 pe "_build/cli localhost 5556 credentials/ca.pem"
 
+clear
+
 pei "_build/cli --coalescing 3 localhost 5556 credentials/ca.pem"
+
+clear
 
 pei "_build/cli --streams 3 localhost 5556 credentials/ca.pem"
 
