bbf7b2441351cd58b9155dd62de225406f947d70
Daiki Ueno <ueno@gnu.org>
2021-09-10 12:26:23 +0200

    demo-scripts: Error out if not called from the top-level directory

--- demo-scripts/serv.sh
+++ demo-scripts/serv.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if ! test -d demo-scripts; then
+    echo "call this script from the top-level directory"
+    exit 1
+fi
+
 . demo-scripts/demo-magic/demo-magic.sh
 
 DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
